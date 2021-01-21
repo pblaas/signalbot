c5075210d8cfc3391d8a5aac7b9514c13bd88b72
Patrick Blaas <patrick@kite4fun.nl>
2021-01-22 00:50:53 +0100

    adding haiku function

--- botfunctions.py
+++ botfunctions.py
@@ -6,6 +6,7 @@ import random
 import json
 import urllib3
 import shutil
+from random import randint
 
 
 class SwitchCase:
@@ -32,6 +33,7 @@ class SwitchCase:
         !flip
         !chuck
         !gif
+        !haiku
         """
 
     def test(self):
@@ -70,3 +72,27 @@ class SwitchCase:
     def random(self):
         """Return a random number between 0 and 1000."""
         return str(random.randrange(1000))
+
+    def haiku(self):
+        """Return a random generator Haiku."""
+        wordList1 = ["Enchanting", "Amazing", "Colourful", "Delightful", "Delicate"]
+        wordList2 = ["visions", "distance", "conscience", "process", "chaos"]
+        wordList3 = ["superstitious", "contrasting", "graceful", "inviting", "contradicting", "overwhelming"]
+        wordList4 = ["true", "dark", "cold", "warm", "great"]
+        wordList5 = ["scenery", "season", "colours", "lights", "Spring", "Winter", "Summer", "Autumn"]
+        wordList6 = ["undeniable", "beautiful", "irreplaceable", "unbelievable", "irrevocable"]
+        wordList7 = ["inspiration", "imagination", "wisdom", "thoughts"]
+
+        wordIndex1 = randint(0, len(wordList1) - 1)
+        wordIndex2 = randint(0, len(wordList2) - 1)
+        wordIndex3 = randint(0, len(wordList3) - 1)
+        wordIndex4 = randint(0, len(wordList4) - 1)
+        wordIndex5 = randint(0, len(wordList5) - 1)
+        wordIndex6 = randint(0, len(wordList6) - 1)
+        wordIndex7 = randint(0, len(wordList7) - 1)
+
+        haiku = wordList1[wordIndex1] + " " + wordList2[wordIndex2] + ",\n"
+        haiku = haiku + wordList3[wordIndex3] + " " + wordList4[wordIndex4] + " " + wordList5[wordIndex5] + ",\n"
+        haiku = haiku + wordList6[wordIndex6] + " " + wordList7[wordIndex7] + "."
+
+        return haiku
