6a611c114eb61c9818895e58087d6342c1f587fc
Patrick Blaas <patrick@kite4fun.nl>
2021-01-22 00:08:32 +0100

    Fix docstyle

--- message.py
+++ message.py
@@ -1,21 +1,23 @@
-""" Message module """
+"""Message module."""
 
 
 class Message:
-    """ Message class to model data """
+    """Message class to model data."""
+
     def __init__(self, source, message, groupinfo):
+        """Initialize Message object with source, message and groupinfo."""
         self._source = source
         self._message = message
         self._groupinfo = groupinfo
 
     def getgroupinfo(self):
-        """ return groupID """
+        """Return groupID."""
         return self._groupinfo
 
     def getmessage(self):
-        """ return message string """
+        """Return message string."""
         return self._message
 
     def getsource(self):
-        """ return initiator of the message """
+        """Return initiator of the message."""
         return self._source
