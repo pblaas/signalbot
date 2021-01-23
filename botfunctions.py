2e6086c664a39a07d54603a8da2978ac7809d3ff
Patrick Blaas <patrick@kite4fun.nl>
2021-01-24 00:18:15 +0100

    flake8 lint

--- botfunctions.py
+++ botfunctions.py
@@ -112,16 +112,15 @@ class SwitchCase:
     names = random
 
     def me(self):
-        """Return random Emoij"""
+        """Return random Emoij."""
         thumb = emoji.emojize(':eggplant:')
         return thumb
-    
+
     def hn(self):
         """Return 1 of 10 latest hacker news items."""
         http = urllib3.PoolManager()
         req_url = http.request('GET', 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
         data = json.loads(req_url.data.decode('utf-8'))
-        selection = data[:10]
         story_url = http.request('GET', 'https://hacker-news.firebaseio.com/v0/item/' + str(data[randint(0, 9)]) + '.json?print=pretty')
         story_data = json.loads(story_url.data.decode('utf-8'))
         return ("HN: [" + str(story_data['score']) + "] " + story_data['title'] + " -> " + story_data['url'])
