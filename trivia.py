78ffcbc94dfc7f22597f623e5915e42cc2bdef04
Patrick Blaas <patrick@kite4fun.nl>
2021-01-25 16:03:23 +0100

    simple trivia test module

---
+++ trivia.py
@@ -0,0 +1,24 @@
+"""Collection of Bot functions."""
+
+import random
+import json
+import urllib3
+
+
+def trivia():
+    """Get random jokes from chucknorris API."""
+    http = urllib3.PoolManager()
+    req_return = http.request('GET', 'https://opentdb.com/api.php?amount=1')
+    trivia_data = json.loads(req_return.data.decode('utf-8'))
+    all_answers = trivia_data['results'][0]['incorrect_answers']
+    all_answers.insert(0, trivia_data['results'][0]['correct_answer'])
+    random.shuffle(all_answers)
+    str = ","
+    shuffled_string = str.join(all_answers)
+    return f"""Trivia:
+    {trivia_data['results'][0]['question']}
+    Options: {shuffled_string}
+    """
+
+
+trivia()
