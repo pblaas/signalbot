e34cf739d2cf66e52a94fcec5b9c3eccaa8381ab
Patrick Blaas <patrick@kite4fun.nl>
2021-01-27 13:43:15 +0100

    adding gnews test module

---
+++ gnews.py
@@ -0,0 +1,40 @@
+"""Simple GNews API module."""
+
+import json
+import urllib3
+import random
+# import pprint
+
+
+class SwitchCaseGnews:
+    """SwitchCaseGnews class to switch gnews bot subfunctions."""
+
+    def __init__(self, query):
+        """Initialize SwitchCase with version and author variables."""
+        self._query = query
+
+    def query(self):
+        """Switch function to switch between available functions."""
+        default = """gnews subcommands:
+        yourquery
+        """
+        return getattr(self, 'fetch', lambda: default)()
+
+    def fetch(self):
+        """Get news from gnews API."""
+        APITOKEN = '3zhnLmjmyvvmVYHF1gB8m8z2WdjDtGjPyqvUOdXMeOxTkkNYnB84on9YGMzQ'
+        reqinfo = self._query
+        http = urllib3.PoolManager()
+        req_return = http.request('GET', 'https://gnewsapi.net/api/search?q=' + reqinfo + '&country=nl&language=nl&api_token=' + APITOKEN)
+        all_news = json.loads(req_return.data.decode('utf-8'))
+        total_articles = len(all_news['articles'])
+        random_article_number = random.randint(0, total_articles)
+        return(f"""
+        {all_news['articles'][random_article_number]['title']} -> {all_news['articles'][random_article_number]['article_url']}
+        """)
+        # return al['value']
+
+
+news = SwitchCaseGnews('Amsterdam')
+newsmessage = news.query().replace('"', '')
+print(newsmessage)
