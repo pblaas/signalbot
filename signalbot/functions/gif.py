d80cd5af15c83cc2c358694c75fc59723def7129
Patrick Blaas <patrick@kite4fun.nl>
2021-01-29 13:34:27 +0100

    Check gif function for valid response

--- signalbot/functions/gif.py
+++ signalbot/functions/gif.py
@@ -11,18 +11,24 @@ class Gif:
 
     def gif(self):
         """Get random gif images from Giphy platform."""
+
         http = urllib3.PoolManager()
         req_gif = http.request('GET', 'https://api.giphy.com/v1/gifs/random?api_key=elRcLdk25G3cllhDMki4ZIKLMxKqRPSW&tag=funny&rating=pg-13')
         gif = json.loads(req_gif.data.decode('utf-8'))
-        url = "https://i.giphy.com/media/" + gif['data']['id'] + "/giphy.gif"
 
-        if self._signalexecutorlocal is False:
-            home = os.environ['HOME']
-            with open(home + "/signal/giphy.gif", 'wb') as out:
-                r = http.request('GET', url, preload_content=False)
-                shutil.copyfileobj(r, out)
+        if len(gif['data']) > 0:
+            url = "https://i.giphy.com/media/" + gif['data']['id'] + "/giphy.gif"
+            if self._signalexecutorlocal is False:
+                home = os.environ['HOME']
+                with open(home + "/signal/giphy.gif", 'wb') as out:
+                    r = http.request('GET', url, preload_content=False)
+                    shutil.copyfileobj(r, out)
+            else:
+                with open("/tmp/signal/giphy.gif", 'wb') as out:
+                    r = http.request('GET', url, preload_content=False)
+                    shutil.copyfileobj(r, out)
+
+            return "Gif"
+
         else:
-            with open("/tmp/signal/giphy.gif", 'wb') as out:
-                r = http.request('GET', url, preload_content=False)
-                shutil.copyfileobj(r, out)
-        return "Gif"
+            return "No valid repsonse for Giphy API."
