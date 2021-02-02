d1af938a00aceb9279e533361abb4f91d9ef96f4
Patrick Blaas <patrick@kite4fun.nl>
2021-02-02 19:58:18 +0100

    handle invalid properly

--- signalbot/functions/hn.py
+++ signalbot/functions/hn.py
@@ -15,6 +15,12 @@ class Hn:
         http = urllib3.PoolManager()
         req_url = http.request('GET', 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
         data = json.loads(req_url.data.decode('utf-8'))
-        story_url = http.request('GET', 'https://hacker-news.firebaseio.com/v0/item/' + str(data[randint(0, 9)]) + '.json?print=pretty')
-        story_data = json.loads(story_url.data.decode('utf-8'))
-        return (thumb + " Hacker News: [" + str(story_data['score']) + "] " + story_data['title'] + " -> " + story_data['url'])
+        if len(data) > 0:
+            story_url = http.request('GET', 'https://hacker-news.firebaseio.com/v0/item/' + str(data[randint(0, 9)]) + '.json?print=pretty')
+            story_data = json.loads(story_url.data.decode('utf-8'))
+            if 'url' in story_data:
+                return thumb + " Hacker News: [" + str(story_data['score']) + "] " + story_data['title'] + " -> " + story_data['url']
+            else:
+                return "Hacker News: Unable to retrieve news."
+        else:
+            return "Hacker News: unable to access api."
