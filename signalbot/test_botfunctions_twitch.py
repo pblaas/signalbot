0173b7518f9a309e06035b8ec3fb848709a0cdaf
Patrick Blaas <patrick@kite4fun.nl>
2021-02-09 15:50:18 +0100

    move switch testcases to its own file

---
+++ signalbot/test_botfunctions_twitch.py
@@ -0,0 +1,34 @@
+from botfunctions import SwitchCase
+from functions.twitch import SwitchCaseTwitch
+import pytest
+import re
+
+
+@pytest.fixture
+def switchcase():
+    return SwitchCase('7.7.7', 'Patrick', True, '')
+
+
+@pytest.fixture
+def switchcasetwitch():
+    return SwitchCaseTwitch()
+
+
+def test_switch_get_function_twitch_without_option(switchcase):
+    assert 'twitch subcommands' in switchcase.switch('!twitch')
+
+    
+def test_switch_get_function_twitch_get_access_token(switchcasetwitch):
+    assert re.match(r'\w{30}', switchcasetwitch._getaccestoken())
+
+
+def test_switch_get_function_twitch_get_topgames(switchcasetwitch):
+    assert re.match(r'\s*Top games:', switchcasetwitch.topgames())
+
+
+def test_switch_get_function_twitch_get_topstreams(switchcasetwitch):
+    assert re.match(r'\s*Top Streams:', switchcasetwitch.topstreams())
+
+
+def test_switch_get_function_twitch_get_pcreleases(switchcasetwitch):
+    assert re.match(r'\s*New PC releases:', switchcasetwitch.pcreleases())
