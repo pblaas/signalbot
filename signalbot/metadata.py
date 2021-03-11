038d4aed550c1e990c84e1d2d640abe4c0de4e08
Patrick Blaas <patrick@kite4fun.nl>
2021-03-11 22:58:42 +0100

    bump minor due to new functionality

--- signalbot/metadata.py
+++ signalbot/metadata.py
@@ -1,3 +1,3 @@
 author = "Patrick Blaas"  # pragma: no cover
 authoremail = "<patrick@kite4fun.nl>"  # pragma: no cover
-version = "0.1.9"  # pragma: no cover
+version = "0.1.10"  # pragma: no cover
