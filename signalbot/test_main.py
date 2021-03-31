faec4f6f0b4211df39e9e5f178327781c268f1f9
Patrick Blaas <patrick@kite4fun.nl>
2021-03-31 05:13:32 +0200

    adding test case in main module

--- signalbot/test_main.py
+++ signalbot/test_main.py
@@ -5,6 +5,12 @@ def test_main_init_program():
     assert signalbot.__main__.init_program() is None
 
 
+def test_main_init_program_no_signalexecutorlocal(monkeypatch):
+    """Main program signalexecutorlocal set to true"""
+    monkeypatch.setenv("SIGNALEXECUTORLOCAL", "True")
+    assert signalbot.__main__.init_program() is None
+
+
 def test_main_parse_message_sentmessage():
     json = '{"envelope":{"source":"+31612345678","sourceDevice":1,"timestamp":1612267167202,"syncMessage":{"sentMessage":{"timestamp":1612267167202,"message":"Test","expiresInSeconds":0,"viewOnce":false,"mentions":[],"attachments":[],"groupInfo":{"groupId":"FmtNZAff32cV5sin4iJKcrikFd67H1LbkLsdXaSVPkI=","type":"DELIVER"},"destination":null}}}}'
     assert signalbot.__main__.parse_message(json) is None
