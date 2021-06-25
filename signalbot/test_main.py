8764159c260c30a9ef7cf4d4d5d3b63f7ce31bb1
Patrick Blaas <patrick@kite4fun.nl>
2021-06-26 00:28:33 +0200

    adding main class test

--- signalbot/test_main.py
+++ signalbot/test_main.py
@@ -10,6 +10,10 @@ def test_main_init_program_no_signalexecutorlocal(monkeypatch):
     monkeypatch.setenv("SIGNALEXECUTORLOCAL", "True")
     assert signalbot.__main__.init_program() is None
 
+def test_main_init_program_debug_true(monkeypatch):
+    """Main program signalexecutorlocal set to true"""
+    monkeypatch.setenv("DEBUG", "True")
+    assert signalbot.__main__.init_program() is None
 
 def test_main_parse_message_sentmessage():
     json = '{"envelope":{"source":"+31612345678","sourceDevice":1,"timestamp":1612267167202,"syncMessage":{"sentMessage":{"timestamp":1612267167202,"message":"Test","expiresInSeconds":0,"viewOnce":false,"mentions":[],"attachments":[],"groupInfo":{"groupId":"FmtNZAff32cV5sin4iJKcrikFd67H1LbkLsdXaSVPkI=","type":"DELIVER"},"destination":null}}}}'
